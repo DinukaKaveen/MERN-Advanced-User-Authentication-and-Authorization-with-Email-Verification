a4e75be0727cc910c5ffbc563df1a858facdaae1
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-11-28 13:48:00 +0530

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("DB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("DB Connection Error: ", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`App is running on PORT ${PORT}`);
