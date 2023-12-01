62b590c6a8b623bbef9cd0e5556ec9c07d7762b3
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-01 12:56:47 +0530

    jk

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -12,26 +12,26 @@ router.post("/register", async (req, res) => {
   try {
     const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
-      return res.status(400).json({message: "Email Already Registered!"});
+      return res.status(400).json({success: false, message: "Email Already Registered!"});
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
-      fullName: req.body.fullName,
+      name: req.body.name,
       email: req.body.email,
       password: hashedPassword,
       emailToken: crypto.randomBytes(64).toString("hex"),
       isVerified: false,
     });
     await user.save();
-    res.status(200).json({message: "User Registered Successfully"});
+    res.status(200).json({success: true, message: "User Registered Successfully"});
 
 
 
   } catch (error) {
     console.log(error);
-    res.status(500).json({message: "Internel Server Error"});
+    res.status(500).json({success: false, message: "Internel Server Error"});
   }
 });
