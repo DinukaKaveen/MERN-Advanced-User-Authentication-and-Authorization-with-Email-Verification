172c7fcdccf67dfd76307997837fbe1f52f25ad1
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-03 16:00:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserLogin from './pages/UserLogin';
-import UserRegister from './pages/UserRegister';
+import UserLogin from "./pages/UserLogin";
+import UserRegister from "./pages/UserRegister";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <div>
         <Routes>
-          <Route path='/' exact element={<UserLogin/>}></Route>
-          <Route path='/register' exact element={<UserRegister/>}></Route>
+          <Route path="/" exact element={<UserLogin />}></Route>
+          <Route path="/dashboard" exact element={<Dashboard />}></Route>
+          <Route path="/register" exact element={<UserRegister />}></Route>
         </Routes>
       </div>
-      </BrowserRouter>
-
+    </BrowserRouter>
   );
 }
 
