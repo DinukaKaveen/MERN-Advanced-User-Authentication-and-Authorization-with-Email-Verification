b2dc2b5635ee0a44eef4482548153817f291bd67
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-03 15:03:59 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,6 +18,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    emailToken: {
+        type: String
+    },
     verified: {
         type: Boolean,
         default: false
