5690d5e5deba7188a64300a2bc1a410cf15f457d
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-05 12:52:44 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLogin from "./pages/UserLogin";
 import UserRegister from "./pages/UserRegister";
 import Dashboard from "./pages/Dashboard";
+import UserVerify from "./pages/UserVerify";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" exact element={<UserLogin />}></Route>
           <Route path="/dashboard" exact element={<Dashboard />}></Route>
           <Route path="/register" exact element={<UserRegister />}></Route>
+          <Route path="/:id/verify/:token" exact element={<UserVerify />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
