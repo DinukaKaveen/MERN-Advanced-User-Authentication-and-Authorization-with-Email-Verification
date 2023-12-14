546ab804f5af14b218136a591c2fb7655c1ea6d0
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-12-14 08:57:30 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-
+const userSchema = new mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     lastName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     emailToken: {
-        type: String
+      type: String,
     },
     verified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model("users", userSchema);
